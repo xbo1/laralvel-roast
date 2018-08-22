f7bff6239ccc41b78ed88edf14425ac0e6171ef1
bob <wangshyn@gmail.com>
2018-08-22 18:07:43 +0800

    参考 http://laravelacademy.org/post/9498.html 完成

---
+++ app/Http/Controllers/Web/AppController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bob
+ * Date: 2018/8/22
+ * Time: 下午6:06
+ */
+
+namespace App\Http\Controllers\Web;
+
+
+use App\Http\Controllers\Controller;
+
+class AppController extends Controller
+{
+    public function getApp() {
+        return view('app');
+    }
+}
